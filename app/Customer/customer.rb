badfc76df449311fc1afe0a1713cef0a50f5ed4f
lars <larsburgess@gmail.com>
2010-06-02 17:33:21 -0700

    * setting up with demo server

--- app/Customer/customer.rb
+++ app/Customer/customer.rb
@@ -1,15 +1,6 @@
 class Customer
-    include Rhom::PropertyBag
+  include Rhom::PropertyBag
 
-    enable :sync
-
-    set :partition, :application
-    set :source_id, 1
-    set :sync_priority, 1
-    #set :sync_type, :bulk_only
-    
-    belongs_to :product_id, 'Product'
-
-    property :image_url, :blob
+  enable :sync
 
 end
--- app/Product/product.rb
+++ app/Product/product.rb
@@ -1,25 +1,5 @@
 class Product
-    include Rhom::FixedSchema
+  include Rhom::PropertyBag
 
-    enable :sync
-
-    set :partition, :application
-    set :source_id, 2
-    set :sync_priority, 0
-    #set :sync_type, :bulk_only
-
-    property :brand, :string
-    property :created_at
-    property :name
-    property :price
-    property :quantity
-    property :sku
-    property :updated_at
-    
-    property :image_url, :blob
-    
-    index :brand, :price
-    unique_index :name
-    
-    set :schema_version, '1.0'
+  enable :sync
 end
